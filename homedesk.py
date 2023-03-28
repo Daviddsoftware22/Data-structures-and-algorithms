ff85a6e7665389a9ad59b3a4af7bdce1383e74c5
Daviddsoftware22 <85884624+Daviddsoftware22@users.noreply.github.com>
2023-03-28 22:20:30 +0300

    Create homedesk.py

---
+++ homedesk.py
@@ -0,0 +1,19 @@
+import time
+from playsound import playsound
+import random
+import math
+import os
+import webbrowser
+
+def turn_off(y):
+    print(f"From now {time.ctime()}")
+    if y > 60 :
+        print(f"Your computer will shut down in {math.trunc(y / 60)} minutes")
+    else:
+        print("Your computer will shut down in less than 0 minutes")
+    while y > 0 :
+        y -= 1
+        time.sleep(1)
+        if y < 30:
+            print(f"Your computer will shut down in : {y} seconds")
+    os.system("shutdown /s /t 1")
